85e6cc3c25166761ced42a1d633bc7b71c37cd7e
Nizar Belkacem <nizar.belkacem17@gmail.com>
2021-03-25 11:01:10 +0100

    feat: real time app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+import { useEffect, useState } from "react";
+import { Button, Paper, TextField, Typography } from "@material-ui/core";
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setnewMessage] = useState('')
+  const [toSend, setToSend] = useState("");
+  useEffect(() => {
+    const socketIo = socketIOClient("127.0.0.1:1025")
+    console.log("Connecting")
+    console.log(socketIo.connected);
+    socketIo.on('connect', socket => {
+      console.log('connnected to socket', socket);
+    });
+    socketIo.on("conversation", (msg) => {
+      
+      setnewMessage(msg);
+    });
+    return () => socketIo.disconnect()
+  }, []);
+  useEffect(() => {
+    setMessages([...messages,newMessage])
+    
+  }, [newMessage])
+  const sendMessage = () => {
+    console.log(toSend)
+    const socketIo = socketIOClient("127.0.0.1:1025")
+
+    socketIo.emit('message', toSend);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor:"#f5f5f5"}}>
+      <Paper style={{margin:'auto' ,minHeight:500,maxHeight:600,minWidth:400,maxWidth:500}}>
+        <div style={{minHeight:500,maxHeight:600,minWidth:400,maxWidth:500}}>
+          {messages?.map((e,i) => {
+            return <Typography key={i}>{e}</Typography>;
+          })}
+        </div>
+        <TextField
+          label="Message"
+          value={toSend}
+          type="string"
+          multiline
+          rows='4'
+          style={{
+            minWidth:400
+          }}
+          onChange={(e) => setToSend(e.target.value)}
+        />
+     
+      </Paper>
+      <Button  style={{marginTop:40}} color="primary" variant={"contained"} onClick={sendMessage}>
+          {" "}
+          Send{" "}
+        </Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App style={{backgroundColor:"#f5f5f5"}} />
   </React.StrictMode>,
   document.getElementById('root')
 );
